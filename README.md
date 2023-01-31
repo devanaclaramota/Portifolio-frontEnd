c6ffa9e30e9fe52228c8f35f15a03d523d350e3f
Ana clara <dev.anaclaramota@outlook.com>
2023-01-31 12:21:33 -0300

    version-desktop

--- README.md
+++ README.md
@@ -1,10 +1,3 @@
 # Portifolio-frontEnd
 
  Construindo um portifólio front-end, construido para ser, direto, contendo meus melhores projetos e mostrando um pouco do que sei.
-
-home
-contato
-sobre mim (com redes sociais)
-conhecimentos 
-projetos
-footer
---
+++ scripts.js
@@ -0,0 +1,36 @@
+const h1 = document.querySelector('.txt');
+
+function digitacao(texto, contador) {
+  if (contador < texto.length) {
+    setTimeout(() => {
+      h1.textContent += texto.charAt(contador);
+      contador++;
+      digitacao(texto, contador);
+    }, 89);
+  }
+}
+
+digitacao("Oi, sou Ana. Desenvolvedora front-end.", 0);
+
+//animação da pagina
+
+const anime = document.querySelectorAll('[data-anime]');
+const animeClass =  'animate'
+
+
+function animeScroll(){
+    const topScroll = window.pageYOffset + (window.innerHeight * 3 /4);
+    anime.forEach(function(element){
+        if(topScroll > element.offsetTop){
+            element.classList.add(animeClass)
+        }else{
+            element.classList.remove(animeClass)
+        }
+    })
+}
+
+
+window.addEventListener('scroll', function(){
+    animeScroll();
+
+})
